85b7b2faf8a78837978f97de193b45bad99bc8c7
Greg Allan <gregory.j.allan@gmail.com>
2012-11-28 10:12:25 -0700

    Making numbers really numbers, not just strings

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 "use strict";
 
-var parse = function(log) {
+function parse(log, cb) {
   var logs = log.split('\n')
     , parsedLogs = []
     , bracketRegEx = /\[(.*?)\]/
     , quoteRegex = /\"(.*?)\"/
     ;
 
-  logs.forEach(function(logString, index) {
+
+  for(var i = 0; i < logs.length; i++) {
+    var logString = logs[i];
+    if(logString.length == 0) continue;
     var time = bracketRegEx.exec(logString)[1];
     time = time.replace(/\//g, ' ');
     time = time.replace(/:/, ' ');
@@ -23,32 +26,46 @@ var parse = function(log) {
     var userAgent = quoteRegex.exec(logString)[1];
     logString = logString.replace(quoteRegex, '');
 
-    var logStringSplit = logString.split(' ');
+    var logStringSplit = logString.split(' ')
+      , bucketOwner    = logStringSplit[0]
+      , bucket         = logStringSplit[1]
+      , remoteIp       = logStringSplit[3]
+      , requestor      = logStringSplit[4]
+      , requestId      = logStringSplit[5]
+      , operation      = logStringSplit[6] + ' ' + logStringSplit[7]
+      , statusCode     = logStringSplit[9]
+      , errorCode      = logStringSplit[10]
+      , bytesSent      = logStringSplit[11]
+      , objectSize     = logStringSplit[12]
+      , totalTime      = logStringSplit[13]
+      , turnAroundTime = logStringSplit[14]
+      , ctime          = logStringSplit[17]
+      ;
 
     var log = {
-      bucketOwner:    logStringSplit[0],
-      bucket:         logStringSplit[1],
+      bucketOwner:    bucketOwner,
+      bucket:         bucket,
       time:           time,
-      remoteIp:       logStringSplit[3],
-      requestor:      logStringSplit[4],
-      requestId:      logStringSplit[5],
-      operation:      logStringSplit[6],
+      remoteIp:       remoteIp,
+      requestor:      requestor,
+      requestId:      requestId,
+      operation:      operation,
       requestUri:     requestUri,
-      httpStatus:     logStringSplit[9],
-      errorCode:      logStringSplit[10],
-      bytesSent:      logStringSplit[11],
-      objectSize:     logStringSplit[12],
-      totalTime:      logStringSplit[13],
-      turnAroundTime: logStringSplit[14],
+      statusCode:     (statusCode == '-' ? statusCode : parseInt(statusCode, 10)),
+      errorCode:      errorCode,
+      bytesSent:      (bytesSent == '-' ? bytesSent : parseInt(bytesSent, 10)),
+      objectSize:     (objectSize == '-' ? objectSize : parseInt(objectSize, 10)),
+      totalTime:      (totalTime == '-' ? totalTime : parseInt(totalTime, 10)),
+      turnAroundTime: (turnAroundTime == '-' ? turnAroundTime : parseInt(turnAroundTime, 10)),
       referrer:       referrer,
       userAgent:      userAgent,
-      ctime:          logStringSplit[17]
+      ctime:          ctime
     }
 
     parsedLogs.push(log);
-  });
+  };
 
-  return parsedLogs;
+  cb(null, parsedLogs);
 }
 
 module.exports = {
